ca6198420d4167875f94ae2cc83fa8c3f0c577da
Ivan <ivan@yandex.ru>
2019-01-11 12:39:52 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3AsyncAwait/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Threading.Thread
+Imports System.Threading.Tasks
+'синхронно
+' в одно и тоже время с чем-либо; одновременно
+
+
+
+Class MainWindow
+	Private Sub mysub()
+		fffAsync()
+		MsgBox("ready")
+	End Sub
+	Private Async Sub fffAsync() 'асинхронный; не одновременно; в разные моменты времени, разновременно
+		Await Task.Run(Sub() Threading.Thread.Sleep(9000))
+		MsgBox("End fffAsync")
+	End Sub
+End Class
